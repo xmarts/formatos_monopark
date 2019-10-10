1dee406fbf2627309d3699ac454ff8f29fdaab98
Gil <acevedo.4297@gmail.com>
2019-10-10 11:41:44 -0500

    Reporte 21

--- formatos_compra_inventario/models/models.py
+++ formatos_compra_inventario/models/models.py
@@ -12,6 +12,7 @@ class PurchaseOrder(models.Model):
 	tipo_de = fields.Selection([('tipo1', 'Tipo 1'),('tipo2', 'Tipo 2')], string="Tipo de")
 	empleado = fields.Many2one('hr.employee', string="Empleado")
 	num_pedido = fields.Char(string="Número de pedido")
+	fecha_limite_pago = fields.Date(string="Fecha limite de pago")
 
 class ResPartner(models.Model):
 	_inherit = "res.partner.bank"
