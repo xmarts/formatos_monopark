d1a0ce331c73f68f124be387f102cc70b6c0ff11
Gilxmarts <gilberto.santiago@xmarts.com>
2019-10-21 10:45:01 -0500

    Hr

--- formatos_compra_inventario/__manifest__.py
+++ formatos_compra_inventario/__manifest__.py
@@ -21,7 +21,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','purchase','contacts','stock'],
+    'depends': ['base','purchase','contacts','stock','hr'],
 
     # always loaded
     'data': [
