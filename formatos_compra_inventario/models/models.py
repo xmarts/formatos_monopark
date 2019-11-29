4f5f2d6c1fddb59fdfd6bbb0ae4ccd40009303c9
Pablo Osorio <pablo.osorio@xmarts.com>
2019-11-29 09:41:36 -0600

    add field estado_reserva

--- formatos_compra_inventario/models/models.py
+++ formatos_compra_inventario/models/models.py
@@ -40,6 +40,7 @@ class StockPicking(models.Model):
 	obser_recep = fields.Text(string="Observaciones")
 	obser_esp = fields.Text(string="Condiciones especiales")
 	fecha_actual = fields.Char(string="Fecha actual", compute="_compute_fecha_actual")
+	estado_reserva = fields.Selection([('Anticipo','Anticipo'),('Autorizado sin pago','Autorizado sin pago'),('Pago total sin documentos','Pago total sin documentos'),('Listo para entregar','Listo para entregar')],string="Estado de reserva")
 
 	@api.one
 	def _compute_fecha_actual(self):
