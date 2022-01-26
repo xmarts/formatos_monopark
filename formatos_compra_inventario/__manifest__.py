463aa5e452c86ff8af461e936b15cce05e0179c4
Gilberto <gilberto.acevedo.xmarts@gmail.com>
2022-01-26 10:43:08 -0600

    [FIX] Name repo

--- formatos_compra_inventario/__manifest__.py
+++ formatos_compra_inventario/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "formatos_compra_inventario_15",
+    'name': "formatos_compra_inventario",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
