90a97a538288de92568711b6c956e4dcc8a37006
Gilberto <gilberto.acevedo.xmarts@gmail.com>
2022-01-28 17:25:49 -0600

    [FIX] Eliminar cambio devolucion

--- formatos_compra_inventario/models/models.py
+++ formatos_compra_inventario/models/models.py
@@ -58,8 +58,6 @@ class StockPicking(models.Model):
 		('listo_para_recibir','Listo para recibir')],
 		string="Estado de reserva", related='estado_reserva', readonly=True)
 
-	is_devolucion = fields.Boolean(string="¿Es devolución?", default=False) 
-
 	@api.one
 	def _compute_fecha_actual(self):
 		fecha = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -73,29 +71,6 @@ class StockPicking(models.Model):
 			self.estado_reserva="Listo_para_entregar"
 		else:
 			self.estado_reserva=""
-	@api.multi
-	def action_assign(self):
-		""" Check availability of picking moves.
-		This has the effect of changing the state and reserve quants on available moves, and may
-		also impact the state of the picking as it is computed based on move's states.
-		@return: True
-		"""
-		self.filtered(lambda picking: picking.state == 'draft').action_confirm()
-
-		moves = self.mapped('move_lines').filtered(lambda move: move.state not in ('draft', 'cancel', 'done'))
-
-		if not self.is_devolucion:
-			if not moves:
-				raise UserError(_('Nothing to check the availability for.'))
-
-		# If a package level is done when confirmed its location can be different than where it will be reserved.
-		# So we remove the move lines created when confirmed to set quantity done to the new reserved ones.
-		package_level_done = self.mapped('package_level_ids').filtered(lambda pl: pl.is_done and pl.state == 'confirmed')
-		package_level_done.write({'is_done': False})
-		moves._action_assign()
-		package_level_done.write({'is_done': True})
-		return True
-
 
 class StockMove(models.Model):
 	_inherit = "stock.move"
